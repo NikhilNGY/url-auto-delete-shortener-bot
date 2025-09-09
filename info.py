5251d67384a24ad8c643502d45647846b7706127
NIKHIL <65222469+NikhilNGY@users.noreply.github.com>
2025-09-09 21:58:21 +0530

    Update info.py

--- info.py
+++ info.py
@@ -31,8 +31,6 @@ auth_channel = environ.get('AUTH_CHANNEL', '-1001951277428')
 auth_grp = environ.get('AUTH_GROUP')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
-GRP_LNK = environ.get('GRP_LNK', '+x6OfRDdUPrUwZTZl')
-CHNL_LNK = environ.get('CHNL_LNK', '+fDkIGNmk5BU5ODVl')
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "")
