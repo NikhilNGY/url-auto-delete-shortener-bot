41db15eacf2b5e6eba43228a78e40053736d8ddd
NIKHIL <65222469+NikhilNGY@users.noreply.github.com>
2025-09-09 19:11:59 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -771,8 +771,8 @@ async def advantage_spell_chok(msg):
     g_s += await search_gagala(msg.text)
     gs_parsed = []
     if not g_s:
-        k = await msg.reply("I couldn't find any movie in that name.")
-        await asyncio.sleep(8)
+        k = await msg.reply("⁣📌Request Format:\n \n👉Send Only Movie Name & Year Only\n      And\n        Movie Name With Correct Spelling (Search In Google &  Then Send🔍)\n \nExample: Joe 2023\n           Good Night 2024")
+        await asyncio.sleep(900)
         await k.delete()
         return
     regex = re.compile(r".*(imdb|wikipedia).*", re.IGNORECASE)  # look for imdb / wiki results
@@ -800,8 +800,8 @@ async def advantage_spell_chok(msg):
     movielist += [(re.sub(r'(\-|\(|\)|_)', '', i, flags=re.IGNORECASE)).strip() for i in gs_parsed]
     movielist = list(dict.fromkeys(movielist))  # removing duplicates
     if not movielist:
-        k = await msg.reply("I couldn't find anything related to that. Check your spelling")
-        await asyncio.sleep(8)
+        k = await msg.reply("⁣📌Request Format:\n \n👉Send Only Movie Name & Year Only\n      And\n        Movie Name With Correct Spelling (Search In Google &  Then Send🔍)\n \nExample: Joe 2023\n               Good Night 2024")
+        await asyncio.sleep(900)
         await k.delete()
         return
     SPELL_CHECK[msg.id] = movielist
